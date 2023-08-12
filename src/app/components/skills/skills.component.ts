c76f0db62b4a40af7fc7938c006cb3881562b741
javifer89 <122439902+javifer89@users.noreply.github.com>
2023-08-12 02:12:46 +0200

    skills generado

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -1,10 +1,56 @@
 import { Component } from '@angular/core';
+import { Skill } from 'src/app/interfaces/skill.interface';
 
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.css']
+  styleUrls: ['./skills.component.css'],
 })
 export class SkillsComponent {
+  skills: Skill[];
 
+  constructor() {
+    this.skills = [
+      {
+        img: '',
+        title: 'HTML/CSS/BOOTSTRAP',
+        description: '',
+      },
+      {
+        img: '',
+        title: 'JavaScript',
+        description: '',
+      },
+      {
+        img: '',
+        title: 'TypeScript',
+        description: '',
+      },
+      {
+        img: '',
+        title: 'Angular16',
+        description: '',
+      },
+      {
+        img: '',
+        title: 'Node.js + Express',
+        description: '',
+      },
+      {
+        img: '',
+        title: 'Python',
+        description: 'Automatización IT con Python',
+      },
+      {
+        img: '',
+        title: 'MySQL',
+        description: '',
+      },
+      {
+        img: '',
+        title: 'MongoDB',
+        description: '',
+      },
+    ];
+  }
 }
