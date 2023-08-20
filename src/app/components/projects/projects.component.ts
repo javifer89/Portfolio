2bd57ef5d39f709f52f46cef3f166927dd483b19
javifer89 <122439902+javifer89@users.noreply.github.com>
2023-08-20 17:12:46 +0200

    cambios en los proyectos, FALTA  AÑADIR CAPTURAS Y DESCRIPCIONES

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -10,34 +10,44 @@ export class ProjectsComponent {
   projects: Project[]
 
 
-
+// TODO SACAR CAPTURAS DE LOS PROYECTOS
   constructor() {
     this.projects = [
       {
         img: '',
         title: 'Hamburguesería',
         description: 'Proyecto de HTML/CSS para una Hamburguesería',
-        url: ''
+        url: '',
       },
       {
         img: '',
         title: 'Gestor de tareas',
-        description: 'Aplicación web para gestión de tareas con HTML/CSS Y JavaScript',
-        url: ''
+        description:
+          'Aplicación web para gestión de tareas con HTML/CSS Y JavaScript',
+        url: '',
+      },
+      {
+        img: '',
+        title: 'Blog',
+        description:
+          'Blog para integrar en una página web, con funcionalidad de filtrado por categorías. Angular, html y css.',
+        url: '',
       },
       {
         img: '',
         title: 'Eventia - gestor cultural',
-        description: 'Proyecto fullstack con Angular y Node.js para la creación de un Gestor de espacios culturales',
-        url: ''
+        description:
+          'Proyecto fullstack con Angular y Node.js para la creación de un Gestor de espacios culturales',
+        url: '',
       },
       {
         img: '',
         title: 'Fresaikiwi Fotografia',
-        description: 'Proyecto fullstack con Angular y Node.js para la creación de una página web de un estudio fotográfico',
-        url: ''
-    },
-  ]
+        description:
+          'Proyecto fullstack con Angular y Node.js para la creación de una página web de un estudio fotográfico',
+        url: '',
+      },
+    ];
 }
 
 
